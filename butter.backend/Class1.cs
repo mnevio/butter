6c7e2f6e5df3516015b19497a2c1e5f4d4ad5d17
DabbingTree <moorehappyhomeschool@gmail.com>
2020-03-29 19:44:05 -0400

    Added butter.backend and the Transfer method

---
+++ butter.backend/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace butter.backend
+{
+    public class Backend
+    {
+        public static void Transfer(List<string> inputs, 
+            string AssemblyPath, 
+            string Class, 
+            string Method)
+        {
+            Assembly asm = Assembly.LoadFrom(AssemblyPath);
+            Type t = asm.GetType(AssemblyPath.Replace(".dll", "") + "." + Class);
+            MethodInfo methodInfo;
+            try
+            {
+                methodInfo = t.GetMethod(Method, new Type[] { typeof(List<string>) });
+                if (methodInfo == null)
+                {
+                    // never throw generic Exception - replace this with some other exception type
+                    throw new Exception("Could not find method to transfer.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+
+
+
+            //unknown.Add("yo");
+
+
+            var o = Activator.CreateInstance(t, null);
+
+            object[] parameters = new object[1];
+            parameters[0] = inputs;
+
+            var r = methodInfo.Invoke(o, parameters);
+        }
+
+
+    }
+}
